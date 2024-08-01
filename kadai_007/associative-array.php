d4341f10d82afd3eecbf056504922fae1f41b58f
syunsuke80144 <166561366+syunsuke80144@users.noreply.github.com>
2024-08-02 07:39:08 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charser="utf-8">
+    <title>kadai_007</title>
+  </head>
+  <body>
+    <p>
+      <?php
+      $onion_data = ['name'=>'onion','price'=>200,'weight'=>160];
+      print_r($onion_data);
+      ?>
+    </p>
+  </body>
+</html>
